a56f1b9ea568e8e90281008cb397935354c71c10
coder123-project <83872425+coder123-project@users.noreply.github.com>
2023-05-31 01:05:00 +0530

    Add files via upload

---
+++ enemy.js
@@ -0,0 +1,58 @@
+
+AFRAME.registerComponent("enemy-fireballs", {
+    init: function () {        
+  // add on the function to call shootEnemyMonster
+    },
+    shootEnemyMonster: function () {
+        var scene = document.querySelector("#scene");
+
+        //enemyMonster entity
+        var enemyMonster = document.querySelectorAll(".enemy");   
+            
+        for (var i = 0; i < enemyMonster.length; i++) {
+
+       //create fireballs
+        var fireball = document.createElement("a-entity");
+
+        fireball.setAttribute("class","fireball")
+        fireball.setAttribute("gltf-model", "./models/fireball/scene.gltf");
+        fireball.setAttribute("dynamic-body", { mass: 0 });
+     
+        var pos=enemyMonster[i].getAttribute("position")
+
+        fireball.setAttribute("position", {
+            x: pos.x,
+            y: pos.y,
+            z: pos.z,
+        });
+        fireball.setAttribute("scale", {
+            x: 0.05,
+            y: 0.05,
+            z: 0.05,
+        });
+
+        scene.appendChild(fireball);      
+
+       
+        //set the velocity and it's direction
+      
+        /******************************************************************************************* */
+
+        //check player life
+       
+        //collide event on enemy bullets
+        fireball.addEventListener("collide", function (e) {
+           
+            if (e.detail.body.el.id === "weapon") {               
+         
+
+                
+
+            }
+        });
+
+    }
+    },
+    
+
+});
